63201fd84ddb2652bb0b23b8af00491fcb79781b
markpassawat <passawat.wetch@mail.kmutt.ac.th>
2021-07-08 17:46:23 +0700

    [UPDATE] Profile page

--- rememable/lib/screens/profile.dart
+++ rememable/lib/screens/profile.dart
@@ -41,6 +41,8 @@ class _ProfileState extends State<Profile> with SingleTickerProviderStateMixin {
                       color: Colors.white)),
             ),
             GestureDetector(
+              onTap: () => Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => SecondRoute())),
               child: Container(
                 alignment: Alignment.center,
                 width: MediaQuery.of(context).size.width * 0.75,
@@ -72,34 +74,176 @@ class _ProfileState extends State<Profile> with SingleTickerProviderStateMixin {
                   ),
                 ),
               ),
-              // onTap: () {
-              //   Navigator.pushReplacement(context,
-              //       MaterialPageRoute(builder: (context) => ));
-              // },
             ),
             Container(
               width: MediaQuery.of(context).size.width,
               height: MediaQuery.of(context).size.height * 0.15,
               margin: EdgeInsets.only(top: 400),
-              color: Colors.grey,
+              color: Color(0xFFF3F3F3),
               child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  Container(
-                    margin: EdgeInsets.all(10),
-                    child: Text('Email'),
+                  GestureDetector(
+                    onTap: () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => SecondRoute()),
+                    ),
+                    child: Row(
+                      children: [
+                        Container(
+                            width: MediaQuery.of(context).size.width * 0.15,
+                            alignment: Alignment.centerRight,
+                            child: Icon(
+                              Icons.email,
+                              color: Colors.grey,
+                              size: 24.0,
+                              semanticLabel:
+                                  'Text to announce in accessibility modes',
+                            )),
+                        Container(
+                          width: MediaQuery.of(context).size.width * 0.25,
+                          padding: EdgeInsets.only(left: 10),
+                          alignment: Alignment.center,
+                          // color: Colors.green,
+                          // margin: EdgeInsets.all(10),
+                          child: Text('Email'),
+                        ),
+                        Container(
+                          alignment: Alignment.centerRight,
+                          // color: Colors.blue,
+                          width: MediaQuery.of(context).size.width * 0.5,
+                          // margin: EdgeInsets.all(10),
+                          child: Text('Text from database: email'),
+                        ),
+                        Container(
+                          // color: Colors.yellow,
+                          width: MediaQuery.of(context).size.width * 0.1,
+                          // margin: EdgeInsets.all(10),
+                          child: Icon(
+                            Icons.navigate_next,
+                            color: Colors.grey,
+                            size: 24.0,
+                            semanticLabel:
+                                'Text to announce in accessibility modes',
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
-                  Container(
-                    margin: EdgeInsets.all(10),
-                    child: Text('Name'),
+                  // 2
+                  Row(
+                    children: [
+                      Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            Row(
+                              children: [
+                                Container(
+                                    margin: EdgeInsets.all(10),
+                                    child: Icon(
+                                      Icons.person,
+                                      color: Colors.grey,
+                                      size: 24.0,
+                                      semanticLabel:
+                                          'Text to announce in accessibility modes',
+                                    )),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.all(10),
+                                  child: Text('Name'),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.all(10),
+                                  child: Text('Text from database: email'),
+                                ),
+                              ],
+                            ),
+                            Row(
+                              children: [
+                                Container(
+                                  margin: EdgeInsets.all(10),
+                                  child: Icon(
+                                    Icons.navigate_next,
+                                    color: Colors.grey,
+                                    size: 24.0,
+                                    semanticLabel:
+                                        'Text to announce in accessibility modes',
+                                  ),
+                                )
+                              ],
+                            )
+                          ]),
+                    ],
                   ),
-                  Container(
-                    margin: EdgeInsets.all(10),
-                    child: Text('Log out'),
+                  // 3
+                  Row(
+                    children: [
+                      Row(children: [
+                        Row(
+                          children: [
+                            Container(
+                                margin: EdgeInsets.all(10),
+                                child: Icon(
+                                  Icons.logout,
+                                  color: Colors.grey,
+                                  size: 24.0,
+                                  semanticLabel:
+                                      'Text to announce in accessibility modes',
+                                )),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.all(10),
+                              child: Text('Logout'),
+                            ),
+                          ],
+                        ),
+                        Row(
+                          children: [
+                            Container(
+                              margin: EdgeInsets.all(10),
+                              child: Icon(
+                                Icons.navigate_next,
+                                color: Colors.grey,
+                                size: 24.0,
+                                semanticLabel:
+                                    'Text to announce in accessibility modes',
+                              ),
+                            )
+                          ],
+                        )
+                      ]),
+                    ],
                   )
                 ],
               ),
             )
           ],
+        ),
+      ),
+    );
+  }
+}
+
+class SecondRoute extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text('Go back!'),
         ),
       ),
     );
